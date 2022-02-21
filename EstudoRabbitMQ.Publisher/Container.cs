607e5aa9b7d4d548b0083b09f7b0da1650290cf6
Lucas Lima da Silva <lukesilva.dev@gmail.com>
2022-02-21 18:30:59 -0300

    Add dependency injection container

---
+++ EstudoRabbitMQ.Publisher/Container.cs
@@ -0,0 +1,22 @@
+﻿using EstudoRabbitMQ.Publisher.Configurations;
+using EstudoRabbitMQ.Publisher.MessageBroker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EstudoRabbitMQ.Publisher
+{
+    public static class Container
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var rabbitmqConfig = configuration.GetSection("RabbitMQ").Get<RabbitMqConfigurations>();
+            services.AddSingleton(rabbitmqConfig);
+
+            services.AddScoped<IQueueConnection, QueueConnection>();
+            services.AddScoped<IQueueSetup, QueueSetup>();
+
+            services.AddScoped<IQueuePublisher, QueuePublisher>();
+            return services;
+        }
+    }
+}
