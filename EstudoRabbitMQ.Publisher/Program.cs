80a0abb344e15463891f2f49456a62ac36bcb42b
Lucas Lima da Silva <lukesilva.dev@gmail.com>
2022-02-21 18:31:26 -0300

    Use newly created message broker

--- EstudoRabbitMQ.Publisher/Program.cs
+++ EstudoRabbitMQ.Publisher/Program.cs
@@ -1,45 +1,28 @@
-﻿using Microsoft.Extensions.Configuration;
-using RabbitMQ.Client;
-using System.Text;
+﻿using EstudoRabbitMQ.Publisher;
+using EstudoRabbitMQ.Publisher.MessageBroker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = new ConfigurationBuilder()
                .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                .AddEnvironmentVariables();
 
 IConfiguration configuration = builder.Build();
 
+var services = new ServiceCollection();
+services.ConfigureServices(configuration);
+var serviceProvider = services.BuildServiceProvider();
+var publisher = serviceProvider.GetRequiredService<IQueuePublisher>();
 
-
-var factory = new ConnectionFactory()
+Console.WriteLine("How many messages do you want to send ?");
+int numberOfMessages = 0;
+int.TryParse(Console.ReadLine(), out numberOfMessages);
+for(var i = 0; i < numberOfMessages; i++)
 {
-    HostName = "localhost",
-    Port = 5672,
-    UserName = "testuser123",
-    Password = "P@ssw0rd!"
-};
+    Thread.Sleep(1000);
+    var message = $"Message created at {DateTime.Now}";
 
-using (var connection = factory.CreateConnection())
-{
-    using (var channel = connection.CreateModel())
-    {
-        channel.QueueDeclare(
-            queue: "StudiesQueue",
-            durable: false,
-            exclusive: false,
-            autoDelete: false,
-            arguments: null
-            );
-        Console.WriteLine("How many messages do you want to send ?");
-        int numberOfMessages = 0;
-        int.TryParse(Console.ReadLine(), out numberOfMessages);
-        for(var i = 0; i < numberOfMessages; i++)
-        {
-            Thread.Sleep(1000);
-            var message = $"Message created at {DateTime.Now}";
-            var body = Encoding.UTF8.GetBytes(message);
-            channel.BasicPublish(exchange: "", routingKey: "StudiesQueue", basicProperties: null, body: body);
-            Console.WriteLine($"Message published: {message}");
-        }
-    }
+    publisher.Publish(message);
+    Console.WriteLine($"Message published: {message}");
 }
