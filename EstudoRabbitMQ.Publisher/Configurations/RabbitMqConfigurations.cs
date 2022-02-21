481f84771abcaa8528a345db725389bafec106b5
Lucas Lima da Silva <lukesilva.dev@gmail.com>
2022-02-21 18:28:51 -0300

    Simplify RabbitMQ configurations

--- EstudoRabbitMQ.Publisher/Configurations/RabbitMqConfigurations.cs
+++ EstudoRabbitMQ.Publisher/Configurations/RabbitMqConfigurations.cs
@@ -2,7 +2,14 @@
 {
     public class RabbitMqConfigurations
     {
-        public ConnectionConfigurations Connection { get; set; }
-        public QueueConfigurations Queue { get; set; }
+        public string Hostname { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string QueueName { get; set; }
+        public string ExchangeName { get; set; }
+        public bool Durable { get; set; }
+        public bool Exclusive { get; set; }
+        public bool AutoDelete { get; set; }
     }
 }
