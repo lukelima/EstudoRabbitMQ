220ff05132482543dc5bd1edad115bd78ae5cc93
Lucas Lima da Silva <lukesilva.dev@gmail.com>
2022-02-21 18:30:08 -0300

    Create queue setup class, a class in which queues and exchanges will be initialized

---
+++ EstudoRabbitMQ.Publisher/MessageBroker/QueueSetup.cs
@@ -0,0 +1,29 @@
+﻿using EstudoRabbitMQ.Publisher.Configurations;
+using RabbitMQ.Client;
+
+namespace EstudoRabbitMQ.Publisher.MessageBroker
+{
+    public class QueueSetup : IQueueSetup
+    {
+        private readonly RabbitMqConfigurations _configurations;
+
+        public QueueSetup(RabbitMqConfigurations configurations)
+        {
+            _configurations = configurations;
+        }
+
+        public void Initialize(IModel channel)
+        {
+            channel.ExchangeDeclare(_configurations.ExchangeName, "topic", durable: true, autoDelete: false, arguments: new Dictionary<string, object>
+            {
+                ["x-queue-mode"] = "lazy"
+            });
+
+            channel.QueueDeclare(_configurations.QueueName, durable: true, autoDelete: false, exclusive: false, arguments: new Dictionary<string, object>
+            {
+                ["x-queue-mode"] = "lazy"
+            });
+            channel.QueueBind(_configurations.QueueName, _configurations.ExchangeName, _configurations.QueueName); // todo: adicionar routing key
+        }
+    }
+}
